76e2e959b046a54f4c841be07dd2c9443c90f317
Yan Batista <ybatistasouza@hotmail.com>
2022-03-04 05:06:38 -0300

    created about component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default function About() {
     return(
-        <h1>This is my about component</h1>
+        <div className='infoContainer about'>
+            <h1 className='about--title'>About</h1>
+            <p className='about--text'>I am a frontend developer who loves Javascipt. I have some experience
+            with Ruby on Rails and decided to try out something different. I'm having a lot of fun learning about React with freeCodeCamp!</p>
+        </div>
     );
 }
