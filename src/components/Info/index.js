1ebc96821db9b366369195306db988860d60985b
Yan Batista <ybatistasouza@hotmail.com>
2022-03-04 04:37:04 -0300

    first commit

---
+++ src/components/Info/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import profile from '../../images/raiden.jpg'
+
+export default function Info() {
+    return(
+        <div className='infoContainer'>
+            <img src={profile} alt="profile" className='infoContainer--photo'></img>
+
+            <div className='profileContainer'>
+                <div className='profileContainer--name'>
+                    <h1>Yan Batista</h1>
+                    <h3>Frontend Developer</h3>
+                    <p>yanbatista.website</p>
+                </div>
+
+                <div className='profileContainer--buttons'>
+                    <button className='btn emailButton'><i class="fa-solid fa-envelope"></i> Email</button>
+                    <button className='btn linkedInButton'><i class="fa-brands fa-linkedin"></i> LinkedIn</button>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/Interests/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Interests() {
+    return(
+        <h1>This is my interests component</h1>
+    );
+}
